a628cd9f3a08e63aa6d86c937767823a62daf88d
Sam Wright <oddsam91@gmail.com>
2012-12-05 14:46:17 -0500

    update change log

--- src/net/sudoscript/rucca/changeLogBuilder.java
+++ src/net/sudoscript/rucca/changeLogBuilder.java
@@ -12,7 +12,7 @@ public class changeLogBuilder {
 		addToChangeLog("0.9.2", "No Changes yet! First build!");
 		addToChangeLog("0.9.3", "Just some clean up in the code. Essentially the same as previous build.");
 		addToChangeLog("1.0.0", "Bug Fix: Fixed Authentication Dectection. \n -Welcome to the Android Market!!");
-		addToChangeLog("1.0.1", "Fixed wording for clarity \n -More Security");
+		addToChangeLog("1.0.1", "Improve UI wording \n -More Security");
 		
 		//Add more changes above this comment
 		changelog.add("Change Log:");
